856ea0f61275a981e1552553e5cb0b9b915b631d
salitygen <salitygen@yandex.ru>
2019-03-29 11:28:29 +0300

    add login form

---
+++ site/index.php
@@ -0,0 +1,16 @@
+<?php
+defined('EXEC') or die;
+?>
+<html>
+	<head>
+	<link href="/site/them/css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<form action="/?task=login">
+			<label>login</label>
+			<input type="text" name="login">
+			<label>Password</label>
+			<input type="password" name="password">
+		</form>
+	</body>
+</html>
