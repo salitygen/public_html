d86edab0665190da84586f62d93aa555785d27ae
salitygen <salitygen@yandex.ru>
2019-03-29 15:52:11 +0300

    Update pdo.php

--- config/pdo.php
+++ config/pdo.php
@@ -5,6 +5,7 @@ class dataBase{
 	
 	public function pdo(){
 		
+		global $main;
 		$dsn = "mysql:host=localhost;dbname={$main->dbname};charset=utf8";
 		$opt = [
 			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
