a52b96684443f98d7fb087f382cf125b74e6cd98
salitygen <salitygen@yandex.ru>
2019-03-29 15:07:01 +0300

    Update checkUser.php

--- api/methods/check/checkUser.php
+++ api/methods/check/checkUser.php
@@ -4,11 +4,25 @@ defined('EXEC') or die;
 class checkUser {
 	public function dataBase($data){
 		if(isset($data['login']) && isset($data['password'])){
+			
 			$login = checkInput::strip($data['login']);
+			$searchUserLogin = $db->query("SELECT user_id FROM crm_users WHERE user_login='{$login}'");
+			$sLogin = $searchUserLogin->fetch(PDO::FETCH_OBJ);
+			
+			var_dump($sLogin);
+			
 			$pass = md5(checkInput::strip($data['password']));
+			$searchUserPass = $db->query("SELECT user_id FROM crm_users WHERE user_pass='{$pass}'");
+			$sPass = $searchUserPassword->fetch(PDO::FETCH_OBJ);
+			
+			var_dump($sPass);
+			
 			return true;
+			
 		}else{
+			
 			return false;
+			
 		}
 	}
 }
