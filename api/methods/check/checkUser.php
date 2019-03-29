c33e986cf0d4405b44de708245ca82f0eb93632a
salitygen <salitygen@yandex.ru>
2019-03-29 13:03:01 +0300

    Update checkUser.php

--- api/methods/check/checkUser.php
+++ api/methods/check/checkUser.php
@@ -6,7 +6,7 @@ class checkUser {
 	public function dataBase($data){
 		$a = array();
 		$a[0] = checkInput::strip($data['login']);
-		$a[1] = checkInput::strip($data['pass']);
+		$a[1] = checkInput::strip($data['password']);
 		return $a;
 	}
 }
