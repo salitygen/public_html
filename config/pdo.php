07544cb1329ecfda18d49dba8e38685c3aa060a2
salitygen <salitygen@yandex.ru>
2019-03-29 14:55:02 +0300

    Update pdo.php

--- config/pdo.php
+++ config/pdo.php
@@ -1,4 +1,13 @@
 <?php
 defined('EXEC') or die;
 
+$dsn = "mysql:dbname={$main->dbname};host=localhost";
+
+try {
+    $db = new PDO($dsn, $main->dbUser, $main->dbPass);
+	$db->exec("set names utf8");
+} catch (PDOException $e) {
+    echo 'Error connect database: ' . $e->getMessage();
+}
+
 ?>
