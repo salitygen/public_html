8b1946eb8a3c9d3b76d42d1a765d2f1ca58c1eec
salitygen <salitygen@yandex.ru>
2019-03-29 15:51:32 +0300

    Update pdo.php

--- config/pdo.php
+++ config/pdo.php
@@ -1,14 +1,21 @@
 <?php
 defined('EXEC') or die;
 
-$dsn = "mysql:host=localhost;dbname={$main->dbname};charset=utf8";
-$opt = [
-	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-	PDO::ATTR_EMULATE_PREPARES   => false,
-];
 class dataBase{
-	public $pdo = new PDO($dsn, $main->dbUser, $main->dbPass, $opt);
+	
+	public function pdo(){
+		
+		$dsn = "mysql:host=localhost;dbname={$main->dbname};charset=utf8";
+		$opt = [
+			PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			PDO::ATTR_EMULATE_PREPARES   => false,
+		];
+		
+		$pdo = new PDO($dsn, $main->dbUser, $main->dbPass, $opt);
+		return $pdo;
+		
+	}
 }
 
 ?>
