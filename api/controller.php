077d45ec1e84f23579fe50eed5ccda694fbb63b0
salitygen <salitygen@yandex.ru>
2019-03-29 15:30:25 +0300

    Update controller.php

--- api/controller.php
+++ api/controller.php
@@ -5,7 +5,7 @@ include $main->root.'/api/methods/main.php';
 if(isset($_GET['task'])){
 	$task = checkInput::strip($_GET['task']);
 	if($task == 'login'){
-		$arr = checkUser::dataBase($_POST);
+		$arr = checkUser::dataBase($_POST,$db);
 	}
 }
 
