8570ddd2dc3360da4f2fc870c01cb8dd52a30b05
salitygen <salitygen@yandex.ru>
2019-03-29 15:48:27 +0300

    Change PDO

--- api/controller.php
+++ api/controller.php
@@ -5,7 +5,7 @@ include $main->root.'/api/methods/main.php';
 if(isset($_GET['task'])){
 	$task = checkInput::strip($_GET['task']);
 	if($task == 'login'){
-		$arr = checkUser::dataBase($_POST,$db);
+		$arr = checkUser::dataBase($_POST);
 	}
 }
 
--- api/methods/check/checkUser.php
+++ api/methods/check/checkUser.php
@@ -3,13 +3,13 @@ defined('EXEC') or die;
 
 class checkUser {
 	
-	public function dataBase($data,$db){
+	public function dataBase($data){
 		
 		if(isset($data['login']) && isset($data['password'])){
 			
 			$login = checkInput::strip($data['login']);
 			$pass = md5(checkInput::strip($data['password']));
-			$searchUserLogin = $db->query("SELECT user_id FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
+			$searchUserLogin = dataBase::pdo->query("SELECT user_id FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
 			$sLogin = $searchUserLogin->fetch(PDO::FETCH_ASSOC);
 			
 			if($sLogin['user_id']){
