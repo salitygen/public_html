b3e416e75a4f3e1d48b18f2248d30e406ad8808d
salitygen <salitygen@yandex.ru>
2019-03-30 16:07:43 +0300

    Update Upload.php

---
+++ api/methods/Upload.php
@@ -0,0 +1,27 @@
+<?php
+defined('EXEC') or die;
+
+class Upload {
+	
+	public function imageProfile($data){
+		
+
+		
+	}
+	
+	public function imageOrder($data){
+		
+
+		
+	}
+	
+	public function fileOrder($data){
+		
+
+		
+	}
+	
+}
+
+
+?>
