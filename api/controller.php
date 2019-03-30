96bdbd0701529af92e58d2b611d64168dc7c0e32
salitygen <salitygen@yandex.ru>
2019-03-30 13:50:37 +0300

    Update controller.php

--- api/controller.php
+++ api/controller.php
@@ -5,10 +5,10 @@ include $main->root.'/api/methods/main.php';
 if(isset($_GET['task'])){
 	$task = checkInput::strip($_GET['task']);
 	if($task == 'login'){
-		$arrUser = User::findBase($_POST);
-		if($arrUser){
-			if($arrUser->user_status == 1){
-				Session::create($arrUser->user_id);
+		$user = User::findBase($_POST);
+		if($user){
+			if($user->user_status == 1){
+				Session::create($user->user_id);
 				header('Location: /');
 			}
 		}
