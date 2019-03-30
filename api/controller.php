1caed4a134347e09b1ad4c1a4ff8602a13e63a06
salitygen <salitygen@yandex.ru>
2019-03-30 15:22:56 +0300

    refactory

--- api/controller.php
+++ api/controller.php
@@ -5,7 +5,7 @@ include $main->root.'/api/methods/main.php';
 if(isset($_GET['task'])){
 	$task = Input::sanitise($_GET['task']);
 	if($task == 'login'){
-		$user = User::findBase($_POST);
+		$user = User::find($_POST);
 		if($user){
 			if($user->user_status == 1){
 				Session::create($user->user_id);
--- api/methods/User.php
+++ api/methods/User.php
@@ -3,15 +3,15 @@ defined('EXEC') or die;
 
 class User {
 	
-	public function findBase($data){
+	public function find($data){
 		
 		if(isset($data['login']) && isset($data['password'])){
 			
 			$db = dataBase::pdo();
 			$login = Input::sanitise($data['login']);
 			$pass = md5(Input::sanitise($data['password']));
-			$searchUserLogin = $db->query("SELECT * FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
-			$user = $searchUserLogin->fetch();
+			$searchUser = $db->query("SELECT * FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
+			$user = $searchUser->fetch();
 
 			if($user){
 				return $user;
