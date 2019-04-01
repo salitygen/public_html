9ca612bb716e7173f59b1ee6029e5db5d8c29d27
salitygen <salitygen@yandex.ru>
2019-04-01 16:51:12 +0300

    Update main.php

--- api/methods/main.php
+++ api/methods/main.php
@@ -4,6 +4,6 @@ include $main->root.'/api/methods/Input.php';
 include $main->root.'/api/methods/User.php';
 include $main->root.'/api/methods/Session.php';
 include $main->root.'/api/methods/Upload.php';
-include $main->root.'/api/methods/Workshops.php';
+include $main->root.'/api/methods/Workshop.php';
 include $main->root.'/api/methods/Login.php';
 ?>
