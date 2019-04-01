747f48c451db312565b12eb0800ab63e2ab60f67
salitygen <salitygen@yandex.ru>
2019-04-01 11:15:38 +0300

    Update index.php

--- panel/index.php
+++ panel/index.php
@@ -12,7 +12,7 @@ include $main->root.'/panel/them/controller.php';
 		</div>
 		<div id="dashBoard">
 			<div id="topPanel">
-			<?php Render::view($path.'dashBoard');?>
+			<?php Render::view($path.'dashBoard',array());?>
 			</div>
 		</div>
 	</body>
