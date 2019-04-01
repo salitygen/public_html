730940486fe2872589740ccb49ed13d3e0d717c6
salitygen <salitygen@yandex.ru>
2019-04-01 10:12:29 +0300

    Update User.php

--- api/methods/User.php
+++ api/methods/User.php
@@ -8,13 +8,19 @@ class User {
 		if(isset($data['login']) && isset($data['password'])){
 			
 			$db = dataBase::pdo();
-			$login = Input::sanitise($data['login']);
-			$pass = md5($data['password']);
-			$searchUser = $db->query("SELECT * FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
-			$user = $searchUser->fetch();
+			if(strlen($data['login']) <= 50 && strlen($data['password']) <= 50){
+				
+				$login = Input::sanitise($data['login']);
+				$pass = md5($data['password']);
+				$searchUser = $db->query("SELECT * FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
+				$user = $searchUser->fetch();
 
-			if($user){
-				return $user;
+				if($user){
+					return $user;
+				}else{
+					return false;
+				}
+				
 			}else{
 				return false;
 			}
