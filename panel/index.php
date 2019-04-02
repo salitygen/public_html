f991bb113e6927f932420a93f596e676c97ded38
salitygen <salitygen@yandex.ru>
2019-04-02 17:32:30 +0300

    Update index.php

--- panel/index.php
+++ panel/index.php
@@ -7,11 +7,11 @@ include $main->root.'/views/controller.php';
 	<link href="/panel/css/style.css" rel="stylesheet">
 	<link href="/panel/css/fontello.css" rel="stylesheet">
 	</head>
-	<body>
+	<body <?php if($main->view == 'settings') print 'class="level2"'; ?>>
 		<div id="leftPanel">
 		<?php print Render::module($main,'menu',true);?>
 		</div>
-		<div id="dashBoard" <?php if($main->view == 'settings') print 'class="level2"'; ?>>
+		<div id="dashBoard">
 			<?php print $page;?>
 		</div>
 	</body>
