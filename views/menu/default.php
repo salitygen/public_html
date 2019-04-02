47d843ca65c8441e2baa4180c2227d31a4efdd71
salitygen <salitygen@yandex.ru>
2019-04-02 12:52:15 +0300

    Update default.php

--- views/menu/default.php
+++ views/menu/default.php
@@ -3,13 +3,13 @@ defined('EXEC') or die;
 ?>
 <div class="menu">
 	<ul>
-		<li><a class="icon-gauge" href="/?view=dashboard">Показатели</a></li>
-		<li><a class="icon-th" href="/?view=order">Заказы</a></li>
-		<li><a class="icon-clock" href="/?view=task">Задачи</a></li>
-		<li><a class="icon-rouble" href="/?view=payment">Платежи</a></li>
-		<li><a class="icon-users" href="/?view=contractor">Контрагенты</a></li>
-		<li><a class="icon-cubes" href="/?view=storage">Склад</a></li>
-		<li><a class="icon-chart-area" href="/?view=reports">Отчеты</a></li>
-		<li><a class="icon-sliders" href="/?view=settings">Настройки</a></li>
+		<li><a class="icon-gauge <?php if($view == 'dashboard') print 'active';?>" href="/?view=dashboard">Показатели</a></li>
+		<li><a class="icon-th <?php if($view == 'dashboard') print 'active';?>" href="/?view=order">Заказы</a></li>
+		<li><a class="icon-clock <?php if($view == 'dashboard') print 'active';?>" href="/?view=task">Задачи</a></li>
+		<li><a class="icon-rouble <?php if($view == 'dashboard') print 'active';?>" href="/?view=payment">Платежи</a></li>
+		<li><a class="icon-users <?php if($view == 'dashboard') print 'active';?>" href="/?view=contractor">Контрагенты</a></li>
+		<li><a class="icon-cubes <?php if($view == 'dashboard') print 'active';?>" href="/?view=storage">Склад</a></li>
+		<li><a class="icon-chart-area <?php if($view == 'dashboard') print 'active';?>" href="/?view=reports">Отчеты</a></li>
+		<li><a class="icon-sliders <?php if($view == 'dashboard') print 'active';?>" href="/?view=settings">Настройки</a></li>
 	</ul>
 </div>
