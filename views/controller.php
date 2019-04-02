728f4043376230286c2fece7319e29de62a9f392
salitygen <salitygen@yandex.ru>
2019-04-02 13:50:01 +0300

    refactory

--- views/controller.php
+++ views/controller.php
@@ -1,10 +1,10 @@
 <?php
 defined('EXEC') or die;
 
-/* class Render {
+class Render {
 	
-	public function page($this){
-		$view = $this->path.$this->view.'/default.php';
+	public function page($main){
+		$view = $main->path.$main->view.'/default.php';
 		if(!is_file($view)){
 			return 'view page not exists';
 		}else{
@@ -14,8 +14,8 @@ defined('EXEC') or die;
 		}
 	}
 	
-	public function module($this,$module,$params){
-		$view = $this->path.$module.'/default.php';
+	public function module($main,$module,$params){
+		$view = $main->path.$module.'/default.php';
 		if(!is_file($view)){
 			return 'view module not exists';
 		}else{
@@ -25,20 +25,13 @@ defined('EXEC') or die;
 		}
 	}
 	
-} */
-
-class ThisPage {
-	public $test = '123';
 }
 
-$this = new ThisPage;
-
 if($view){
-	$this->path = $main->root.'/views/';
-	$this->view = $view;
-	$this->params = $params;
-	var_dump($this);
-	//$page = Render::page($this);
+	$main->path = $main->root.'/views/';
+	$main->view = $view;
+	$main->params = $params;
+	$page = Render::page($main);
 }
 
 
