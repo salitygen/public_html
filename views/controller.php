f981e0f6004961890feee0f1fd3fd7ae6240d706
salitygen <salitygen@yandex.ru>
2019-04-02 18:06:17 +0300

    refactory

--- views/controller.php
+++ views/controller.php
@@ -21,6 +21,17 @@ class Render {
 		}
 	}
 	
+	public function view($main){
+		$view = $main->root.'/views/'.$main->view.'/'.$main->params.'.php';
+		if(!is_file($view)){
+			return 'view page not exists';
+		}else{
+			ob_start();
+			include($view);
+			return ob_get_clean();
+		}
+	}
+	
 	public function module($main,$module,$params){
 		$view = $main->root.'/views/'.$module.'/default.php';
 		if(!is_file($view)){
--- views/settings/default.php
+++ views/settings/default.php
@@ -2,3 +2,4 @@
 defined('EXEC') or die;
 ?>
 <h1>Настройки системы</h1>
+<?php print Render::view($main); ?>
