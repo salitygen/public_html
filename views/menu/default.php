75c0521a8530e93d0a6a618536cfedb0b053a342
salitygen <salitygen@yandex.ru>
2019-04-02 11:34:38 +0300

    Update default.php

--- views/menu/default.php
+++ views/menu/default.php
@@ -1,4 +1,15 @@
 <?php
 defined('EXEC') or die;
-print 'leftMenu';
 ?>
+<div class="menu">
+	<ul>
+		<li><a href="/?view=dashboard">Показатели</a></li>
+		<li><a href="/?view=order">Заказы</a></li>
+		<li><a href="/?view=task">Задачи</a></li>
+		<li><a href="/?view=payment">Платежи</a></li>
+		<li><a href="/?view=contractor">Контрагенты</a></li>
+		<li><a href="/?view=storage">Склад</a></li>
+		<li><a href="/?view=reports">Отчеты</a></li>
+		<li><a href="/?view=settings">Настройки</a></li>
+	</ul>
+</div>
