f6ae5fb754bfce4dcbb4390d54a05b68d105d4a3
salitygen <salitygen@yandex.ru>
2019-04-02 12:09:32 +0300

    Update default.php

--- views/menu/default.php
+++ views/menu/default.php
@@ -3,13 +3,13 @@ defined('EXEC') or die;
 ?>
 <div class="menu">
 	<ul>
-		<li><a href="/?view=dashboard">Показатели</a></li>
-		<li><a href="/?view=order">Заказы</a></li>
-		<li><a href="/?view=task">Задачи</a></li>
-		<li><a href="/?view=payment">Платежи</a></li>
-		<li><a href="/?view=contractor">Контрагенты</a></li>
-		<li><a href="/?view=storage">Склад</a></li>
-		<li><a href="/?view=reports">Отчеты</a></li>
-		<li><a href="/?view=settings">Настройки</a></li>
+		<li><a class="icon-cog" href="/?view=dashboard">Показатели</a></li>
+		<li><a class="" href="/?view=order">Заказы</a></li>
+		<li><a class="" href="/?view=task">Задачи</a></li>
+		<li><a class="icon-rouble" href="/?view=payment">Платежи</a></li>
+		<li><a class="" href="/?view=contractor">Контрагенты</a></li>
+		<li><a class="icon-cubes" href="/?view=storage">Склад</a></li>
+		<li><a class="" href="/?view=reports">Отчеты</a></li>
+		<li><a class="" href="/?view=settings">Настройки</a></li>
 	</ul>
 </div>
