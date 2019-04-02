0bfdd99ef91e0d1565421d9be1dc1025e40d8507
salitygen <salitygen@yandex.ru>
2019-04-02 18:28:30 +0300

    Update Input.php

--- api/methods/Input.php
+++ api/methods/Input.php
@@ -15,10 +15,22 @@ class Input {
 		return $text;
 	}
 	
+	public function getSanitise($text){
+		$quotes = array("\x27", "\x22", "\x60", "\t", "\n", "\r","*", "%", "<", ">", "?", "!","/",".");
+		$goodquotes = array('-', '+', '#','"');
+		$repquotes = array("\-", "\+", "\#","&quot;");
+		$text = htmlspecialchars($text);
+		$text = stripslashes($text);
+		$text = trim(strip_tags($text));
+		$text = str_replace($quotes,'',$text);
+		$text = str_replace($repquotes,$goodquotes,$text);
+		return $text;
+	}
+	
 	public function task(){
 		if(isset($_GET['task'])){
 			if(strlen($_GET['task']) <= 50){
-				$task = Input::sanitise($_GET['task']);
+				$task = Input::getSanitise($_GET['task']);
 				return $task;
 			}else{
 				return false;
@@ -31,7 +43,7 @@ class Input {
 	public function view(){
 		if(isset($_GET['view'])){
 			if(strlen($_GET['view']) <= 50 && $_GET['view'] != ''){
-				$view = Input::sanitise($_GET['view']);
+				$view = Input::getSanitise($_GET['view']);
 				return $view;
 			}else{
 				return false;
