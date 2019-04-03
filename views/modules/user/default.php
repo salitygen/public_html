1571140b53596b80f2cdb605266d53a02fd9a32d
salitygen <salitygen@yandex.ru>
2019-04-03 16:51:54 +0300

    new module user profile

---
+++ views/modules/user/default.php
@@ -0,0 +1,16 @@
+<?php
+defined('EXEC') or die;
+?>
+<div class="user">
+	<div class="statusbar"><?php print $main->session->user_statusbar; ?></div>
+	<div class="avatar">
+		<img src="<?php print $main->session->user_avatar; ?>">
+	</div>
+	<div class="userInfo">
+		<p class="username"><?php print $main->session->user_name; ?></p>
+		<p class="username"><?php print $main->session->group_name; ?></p>
+	</div>
+	<div class="buttons">
+		<a class="icon-off" href="/?task=logout">Выйти</a>
+	</div>
+</div>
