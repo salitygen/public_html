61d6b55fcbd0c340ddd46610e0aba76c401a8243
salitygen <salitygen@yandex.ru>
2019-04-05 11:00:59 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -1,11 +1,7 @@
 <?php
 defined('EXEC') or die;
 
-if(!$main->session->group_super_users){
-	if(!$main->session->group_settings_general){
-		die ('Access Denied');
-	}
-}
+
 
 ?>
 <p>Информация о вашей компании</p>
