d78807055b2fa780e0716c3aefd6c15604559463
salitygen <salitygen@yandex.ru>
2019-04-05 11:29:05 +0300

    Update default.php

--- views/modules/menu/default.php
+++ views/modules/menu/default.php
@@ -4,30 +4,30 @@ defined('EXEC') or die;
 <div class="menu">
 	<?php print Render::module($main,'user',true);?>
 	<ul>
-		<?php if(Rules::showCompanyPerformance($main)) : ?>
+		<?php //if(Rules::showCompanyPerformance($main)) : ?>
 		<li><a class="icon-gauge <?php if($main->view == 'dashboard') print 'active';?>" href="/?view=dashboard">Показатели</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeOrders($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeOrders($main)) : ?>
 		<li><a class="icon-th <?php if($main->view == 'order') print 'active';?>" href="/?view=order">Заказы</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeTask($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeTask($main)) : ?>
 		<li><a class="icon-clock <?php if($main->view == 'task') print 'active';?>" href="/?view=task">Задачи</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeShop($main) || Rules::seeTills($main) || Rules::seeReturns($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeShop($main) || Rules::seeTills($main) || Rules::seeReturns($main)) : ?>
 		<li><a class="icon-rouble <?php if($main->view == 'payment') print 'active';?>" href="/?view=payment">Платежи</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeContractors($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeContractors($main)) : ?>
 		<li><a class="icon-users <?php if($main->view == 'contractor') print 'active';?>" href="/?view=contractor">Контрагенты</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeStorage($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeStorage($main)) : ?>
 		<li><a class="icon-cubes <?php if($main->view == 'storage') print 'active';?>" href="/?view=storage">Склад</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeReports($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeReports($main)) : ?>
 		<li><a class="icon-chart-area <?php if($main->view == 'reports') print 'active';?>" href="/?view=reports">Отчеты</a></li>
-		<?php endif;?>
-		<?php if(Rules::seeSettings($main)) : ?>
+		<?php //endif;?>
+		<?php //if(Rules::seeSettings($main)) : ?>
 		<li><a class="icon-sliders <?php if($main->view == 'settings') print 'active';?>" href="/?view=settings&params=general">Настройки</a></li>
-		<?php endif;?>
+		<?php //endif;?>
 	</ul>
 	<?php if($main->view == 'settings' && Rules::seeSettings($main)) : ?>
 		<ul class="level2">
