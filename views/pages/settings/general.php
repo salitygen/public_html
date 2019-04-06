04d5df3868c7f0e257e60984db9638f4b5066316
salitygen <salitygen@yandex.ru>
2019-04-06 13:25:06 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -13,3 +13,41 @@ var_dump($ws);
 
 ?>
 <p>Информация о вашей компании</p>
+<div id="workshopInfo">
+	<form method="POST">
+		<div class="workshopName">
+			<input type="text" required="required" name="workshop_name" value="<?php print $ws->workshop_name; ?>">
+		</div>
+		<div class="workshopNote">
+			<input type="text" name="workshop_note" value="<?php print $ws->workshop_note; ?>">
+		</div>
+		<div class="workshopPhones">
+			<?php if($ws->phones) : ?>
+			<?php foreach($ws->phones as $k => $value):?>
+				<input type="text" name="phones[<?php print $k; ?>]" value="<?php print $value; ?>">
+			<?php endforeach;?>
+			<?php else;?>
+				<input type="text" name="phones[0]" value="" placeholder="">
+			<?php endif;?>
+		</div>
+		<div class="workshopMails">
+			<?php if($ws->mails) : ?>
+			<?php foreach($ws->mails as $k => $value):?>
+				<input type="text" name="mails[<?php print $k; ?>]" value="<?php print $value; ?>">
+			<?php endforeach;?>
+			<?php else;?>
+				<input type="text" name="mails[0]" value="" placeholder="">
+			<?php endif;?>
+		</div>
+		<div class="workshopAddres">
+			<?php if($ws->addres) : ?>
+			<?php foreach($ws->addres as $k => $value):?>
+				<input type="text" name="addres[<?php print $k; ?>]" value="<?php print $value; ?>">
+			<?php endforeach;?>
+			<?php else;?>
+				<input type="text" name="addres[0]" value="" placeholder="">
+			<?php endif;?>
+		</div>
+		<button class="save" type="submit">Сохранить</button>
+	</form>
+</div>
