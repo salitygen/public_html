8d818e8362fd79a869f2c814e225da20e68df505
salitygen <salitygen@yandex.ru>
2019-04-06 12:54:47 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -1,11 +1,14 @@
 <?php
 defined('EXEC') or die;
 Rules::settingsGeneral($main) or die('Access Denied');
+
+//Start info collection
 $ws = new stdClass;
 $ws->data = Workshop::get($main->session->user_workshops_id);
 $ws->addres = Addres::get($ws->data->workshop_addres_id);
 $ws->mails = Mails::get($ws->data->workshop_mail_id);
 $ws->phones = Phone::get($ws->data->workshop_phone_id);
+//End info collection
 
 var_dump($ws);
 
