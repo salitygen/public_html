0de908409cd1d1f6edd716bd6f6da5d316c6dcac
salitygen <salitygen@yandex.ru>
2019-04-06 14:11:21 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -16,36 +16,56 @@ var_dump($ws);
 <div id="workshopInfo">
 	<form method="POST">
 		<div class="workshopName">
+			<label>Название</label>
 			<input type="text" required="required" name="workshop_name" value="<?php print $ws->workshop_name; ?>">
 		</div>
 		<div class="workshopNote">
+			<label>Дополнительная информация</label>
 			<input type="text" name="workshop_note" value="<?php print $ws->workshop_note; ?>">
 		</div>
 		<div class="workshopPhones">
 			<?php if($ws->phones) : ?>
-			<?php foreach($ws->phones as $k => $value):?>
-				<input type="text" name="phones[<?php print $k; ?>]" value="<?php print $value; ?>">
+			<?php foreach($ws->phones as $k => $phone):?>
+			<div class="phoneDiv">
+				<input type="text" name="phones[<?php print $k; ?>][value]" value="<?php print $phone->value; ?>">
+				<input type="text" name="phones[<?php print $k; ?>][note]" value="<?php print $phone->note; ?>">
+			</div>
 			<?php endforeach;?>
 			<?php else:?>
-				<input type="text" name="phones[0]" value="" placeholder="">
+			<div class="phoneDiv">
+				<input type="text" name="phones[0][value]" value="" placeholder="">
+				<input type="text" name="phones[0][note]" value="" placeholder="">
+			</div>
 			<?php endif;?>
 		</div>
 		<div class="workshopMails">
 			<?php if($ws->mails) : ?>
-			<?php foreach($ws->mails as $k => $value):?>
-				<input type="text" name="mails[<?php print $k; ?>]" value="<?php print $value; ?>">
+			<?php foreach($ws->mails as $k => $mail):?>
+			<div class="mailDiv">
+				<input type="text" name="mails[<?php print $k; ?>][value]" value="<?php print $mail->value; ?>">
+				<input type="text" name="mails[<?php print $k; ?>][note]" value="<?php print $mail->note; ?>">
+			</div>
 			<?php endforeach;?>
 			<?php else:?>
-				<input type="text" name="mails[0]" value="" placeholder="">
+			<div class="mailDiv">
+				<input type="text" name="mails[0][value]" value="" placeholder="">
+				<input type="text" name="mails[0][note]" value="" placeholder="">
+			</div>
 			<?php endif;?>
 		</div>
 		<div class="workshopAddres">
 			<?php if($ws->addres) : ?>
-			<?php foreach($ws->addres as $k => $value):?>
-				<input type="text" name="addres[<?php print $k; ?>]" value="<?php print $value; ?>">
+			<?php foreach($ws->addres as $k => $adres):?>
+			<div class="addresDiv">
+				<input type="text" name="addres[<?php print $k; ?>][value]" value="<?php print $adres->value; ?>">
+				<input type="text" name="addres[<?php print $k; ?>][note]" value="<?php print $adres->note; ?>">
+			</div>
 			<?php endforeach;?>
 			<?php else:?>
-				<input type="text" name="addres[0]" value="" placeholder="">
+			<div class="addresDiv">
+				<input type="text" name="addres[0][value]" value="" placeholder="">
+				<input type="text" name="addres[0][note]" value="" placeholder="">
+			</div>
 			<?php endif;?>
 		</div>
 		<button class="save" type="submit">Сохранить</button>
