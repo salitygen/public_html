5f81e5a0e4ebd35638924fd4f5f38d39b0644d75
salitygen <salitygen@yandex.ru>
2019-04-06 12:25:10 +0300

    add method addres

---
+++ api/methods/Addres.php
@@ -0,0 +1,56 @@
+<?php
+defined('EXEC') or die;
+
+class Addres {
+	
+	public function get($id){
+		
+		$db = dataBase::pdo();
+		$getAddres = $db->query("SELECT * FROM crm_addres WHERE addres_id={$id}");
+		$addres = $getAddres->fetch();
+		
+		if($addres){
+			return $addres;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	public function getAll(){
+		
+		$db = dataBase::pdo();
+		$getAddreses = $db->query("SELECT * FROM crm_addres");
+		$addreses = $getAddreses->fetch();
+		
+		if($addres){
+			return $addreses;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	public function add(){
+		//if(Rules::settingsWorkshop($main)){
+			
+		//}
+	}
+	public function update($id){
+		//if(Rules::settingsWorkshop($main)){
+			
+		//}
+	}
+	public function block($id){
+		//if(Rules::settingsWorkshop($main)){
+			
+		//}
+	}
+	public function remove($id){
+		//if(Rules::settingsWorkshop($main)){
+			
+		//}
+	}
+	
+}
+?>
