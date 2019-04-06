e7525c4ab70be39ee6723a70eafdbe94e80fdaf4
salitygen <salitygen@yandex.ru>
2019-04-06 11:43:01 +0300

    Update Workshop.php

--- api/methods/Workshop.php
+++ api/methods/Workshop.php
@@ -2,23 +2,55 @@
 defined('EXEC') or die;
 
 class Workshop {
-	public function get(){
+	
+	public function get($main){
+		
+		$db = dataBase::pdo();
+		$getWorkshop = $db->query("SELECT * FROM crm_workshops WHERE workshop_id=1");
+		$workshop = $getWorkshop->fetch();
+		
+		if($workshop){
+			return $workshop;
+		}else{
+			return false;
+		}
 		
 	}
-	public function getAll(){
+	
+	public function getAll($main){
+		
+		$db = dataBase::pdo();
+		$getWorkshops = $db->query("SELECT * FROM crm_workshops");
+		$workshops = $getWorkshops->fetch();
+		
+		if($workshops){
+			return $workshops;
+		}else{
+			return false;
+		}
 		
 	}
-	public function add(){
-		
+	
+	public function add($main){
+		if(Rules::settingsWorkshop($main)){
+			
+		}
 	}
-	public function update(){
-		
+	public function update($main){
+		if(Rules::settingsWorkshop($main)){
+			
+		}
 	}
-	public function block(){
-		
+	public function block($main){
+		if(Rules::settingsWorkshop($main)){
+			
+		}
 	}
-	public function remove(){
-		
+	public function remove($main){
+		if(Rules::settingsWorkshop($main)){
+			
+		}
 	}
+	
 }
 ?>
