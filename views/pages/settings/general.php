f46c39b6ac978ca7349ceab362b2f3d8ea0b9ca2
salitygen <salitygen@yandex.ru>
2019-04-06 12:51:06 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -1,10 +1,11 @@
 <?php
 defined('EXEC') or die;
 Rules::settingsGeneral($main) or die('Access Denied');
-$ws = new WorkshopData;
+$ws = new stdClass;
 
 $ws->data = Workshop::get($main->session->user_workshops_id);
 $ws->addres = Addres::get($ws->data->workshop_addres_id);
 var_dump($ws);
+
 ?>
 <p>Информация о вашей компании</p>
