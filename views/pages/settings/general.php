5371b8b913b2f753feb5186adc627dfe69ef6c02
salitygen <salitygen@yandex.ru>
2019-04-06 14:30:03 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -1,18 +1,21 @@
 <?php
 defined('EXEC') or die;
 Rules::settingsGeneral($main) or die('Access Denied');
-
 //Start info collection
 $ws = Workshop::get($main->session->user_workshops_id);
 $ws->addres = Addres::get($ws->workshop_addres_id);
 $ws->mails = Mails::get($ws->workshop_mail_id);
 $ws->phones = Phone::get($ws->workshop_phone_id);
 //End info collection
-
-var_dump($ws);
-
 ?>
 <p>Информация о вашей компании</p>
+<div class="companyStatus">
+<?php if($ws->workshop_status):?>
+	<p>Работает</p>
+<?php else:?>
+	<p>Не работает</p>
+<?php endif;?>
+</div>
 <div id="workshopInfo">
 	<form method="POST">
 		<div class="workshopName">
