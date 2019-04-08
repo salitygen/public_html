7df568aa9d1a4df5d4b5814619a7b641dc6dbe31
salitygen <salitygen@yandex.ru>
2019-04-08 13:03:45 +0300

    refactory

--- api/methods/Workshop.php
+++ api/methods/Workshop.php
@@ -10,9 +10,9 @@ class Workshop {
 		$workshop = $getWorkshop->fetch();
 		
 		if($workshop){
-			$workshop->mails = json_decode($workshop->workshop_mail_json, true);
-			$workshop->phones = json_decode($workshop->workshop_phone_json, true);
-			$workshop->addres = json_decode($workshop->workshop_addres_json, true);
+			$workshop->mails = json_decode($workshop->workshop_mail_json);
+			$workshop->phones = json_decode($workshop->workshop_phone_json);
+			$workshop->addres = json_decode($workshop->workshop_addres_json);
 			return $workshop;
 		}else{
 			return false;
--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -3,9 +3,6 @@ defined('EXEC') or die;
 Rules::settingsGeneral($main) or die('Access Denied');
 //Start info collection
 $ws = Workshop::get($main->session->user_workshops_id);
-//$ws->addres = Addres::get($ws->workshop_addres_id);
-//$ws->mails = Mails::get($ws->workshop_mail_id);
-//$ws->phones = Phone::get($ws->workshop_phone_id);
 //End info collection
 ?>
 <p>Информация о вашей компании</p>
