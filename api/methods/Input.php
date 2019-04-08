19f605685963f7a4d0e78b54cf1c2d273ba009d7
salitygen <salitygen@yandex.ru>
2019-04-08 13:37:27 +0300

    Update Input.php

--- api/methods/Input.php
+++ api/methods/Input.php
@@ -35,26 +35,6 @@ class Input {
 		}
 	}
 	
-	public function postSanitise($text){
-		if(isset($text)){
-			$quotes = array("\x27", "\x22", "\x60", "\t", "\n", "\r","*", "%", "<", ">", "?", "!","/",".");
-			$goodquotes = array('-', '+', '#','"');
-			$repquotes = array("\-", "\+", "\#","&quot;");
-			$text = htmlspecialchars($text);
-			$text = stripslashes($text);
-			$text = trim(strip_tags($text));
-			$text = str_replace($quotes,'',$text);
-			$text = str_replace($repquotes,$goodquotes,$text);
-			if($text != ''){
-				return $text;
-			}else{
-				return false;
-			}
-		}else{
-			return false;
-		}
-	}
-	
 	public function task(){
 		if(isset($_GET['task'])){
 			if(strlen($_GET['task']) <= 50){
