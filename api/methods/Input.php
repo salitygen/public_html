d6cfb2f337ff1c70e1863fdcab33f9372b5359c4
salitygen <salitygen@yandex.ru>
2019-04-08 17:54:12 +0300

    Update Input.php

--- api/methods/Input.php
+++ api/methods/Input.php
@@ -20,6 +20,7 @@ class Input {
 	}
 	
 	public function getSanitise($text){
+		is_array($text) $text = 'Array';
 		$quotes = array("\x27", "\x22", "\x60", "\t", "\n", "\r","*", "%", "<", ">", "?", "!","/",".");
 		$goodquotes = array('-', '+', '#','"');
 		$repquotes = array("\-", "\+", "\#","&quot;");
