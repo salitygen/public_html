114d7a24cdcefc7534cf40abd9dc81325bc8ba8c
salitygen <salitygen@yandex.ru>
2019-04-08 15:02:20 +0300

    Update SystemMessage.php

---
+++ api/methods/SystemMessage.php
@@ -0,0 +1,25 @@
+<?php
+defined('EXEC') or die;
+
+class SystemMessage {
+	
+	public function set($type,$mess){
+		if($type && $type !=''){
+			$main->sysmessage->type = $type;
+		}
+		if($mess && $mess !=''){
+			$main->sysmessage->mess = $mess;
+		}
+	}
+	
+	public function get(){
+		if(isset($main->sysmessage)){
+			return "<p class='"{$main->sysmessage->type}"'>"{$main->sysmessage->mess}"<p>";
+		}else{
+			return false;
+		}
+	}
+	
+}
+
+?>
