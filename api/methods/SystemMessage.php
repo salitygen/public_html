36ebcbe569fd6c587fb6308f13d67c6774c2bc62
salitygen <salitygen@yandex.ru>
2019-04-08 15:06:44 +0300

    Update SystemMessage.php

--- api/methods/SystemMessage.php
+++ api/methods/SystemMessage.php
@@ -3,7 +3,7 @@ defined('EXEC') or die;
 
 class SystemMessage {
 	
-	public function set($type,$mess){
+	public function set($type,$mess,$main){
 		if($type && $type !=''){
 			$main->sysmessage->type = $type;
 		}
@@ -12,7 +12,7 @@ class SystemMessage {
 		}
 	}
 	
-	public function get(){
+	public function get($main){
 		if(isset($main->sysmessage)){
 			return '<p class="'.$main->sysmessage->type.'">'.$main->sysmessage->mess.'<p>';
 		}else{
