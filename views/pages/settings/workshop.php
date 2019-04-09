ccf8c81ff15975b854fe00d8410b17c536a3495e
salitygen <salitygen@yandex.ru>
2019-04-09 14:08:29 +0300

    Update workshop.php

--- views/pages/settings/workshop.php
+++ views/pages/settings/workshop.php
@@ -21,7 +21,87 @@ $rules = 1; // прописано жестко
 <?php endif;?>
 </div>
 <?php endif;?>
-
+<div id="workshopList">
+	<?php foreach($ws as $value):?>
+		<div id="workshopInfo">
+			<form action="/?view=settings&params=workshop&task=updateworkshop" method="POST">
+				<div class="center">
+				  <input type="checkbox" name="workshop_status" id="cbx" style="display:none" <?php if($value->workshop_status) print 'checked="true"'; ?> >
+				  <label for="cbx" class="toggle"><span></span></label>    
+				</div>
+				<div class="workshopName">
+					<label>Название</label>
+					<input type="text" required="required" name="workshop_name" value="<?php if($value->workshop_name != '') print $value->workshop_name; ?>">
+				</div>
+				<div class="workshopNote">
+					<label>Дополнительная информация</label>
+					<textarea name="workshop_note" ><?php if($value->workshop_note != '') print $value->workshop_note; ?></textarea>
+				</div>
+				<div class="workshopPhones">
+					<label>Номера телефонов</label>
+					<?php if($value->phones) : ?>
+					<?php foreach($value->phones as $k => $phone):?>
+					<div class="phoneDiv">
+						<label>Телефон</label>
+						<input type="text" name="phones[<?php print $k; ?>][value]" value="<?php print $phone->value; ?>">
+						<label>Комментарий</label>
+						<input type="text" name="phones[<?php print $k; ?>][note]" value="<?php print $phone->note; ?>">
+					</div>
+					<?php endforeach;?>
+					<?php else:?>
+					<div class="phoneDiv">
+						<label>Телефон</label>
+						<input type="text" name="phones[0][value]" value="" placeholder="">
+						<label>Комментарий</label>
+						<input type="text" name="phones[0][note]" value="" placeholder="">
+					</div>
+					<?php endif;?>
+				</div>
+				<div class="workshopMails">
+					<label>Электронная почта</label>
+					<?php if($value->mails) : ?>
+					<?php foreach($value->mails as $k => $mail):?>
+					<div class="mailDiv">
+						<label>Почтовый ящик</label>
+						<input type="text" name="mails[<?php print $k; ?>][value]" value="<?php print $mail->value; ?>">
+						<label>Комментарий</label>
+						<input type="text" name="mails[<?php print $k; ?>][note]" value="<?php print $mail->note; ?>">
+					</div>
+					<?php endforeach;?>
+					<?php else:?>
+					<div class="mailDiv">
+						<label>Почтовый ящик</label>
+						<input type="text" name="mails[0][value]" value="" placeholder="">
+						<label>Комментарий</label>
+						<input type="text" name="mails[0][note]" value="" placeholder="">
+					</div>
+					<?php endif;?>
+				</div>
+				<div class="workshopAddres">
+					<label>Адреса компании</label>
+					<?php if($value->addres) : ?>
+					<?php foreach($value->addres as $k => $adres):?>
+					<div class="addresDiv">
+						<label>Адрес</label>
+						<input type="text" name="addres[<?php print $k; ?>][value]" value="<?php print $adres->value; ?>">
+						<label>Комментарий</label>
+						<input type="text" name="addres[<?php print $k; ?>][note]" value="<?php print $adres->note; ?>">
+					</div>
+					<?php endforeach;?>
+					<?php else:?>
+					<div class="addresDiv">
+						<label>Адрес</label>
+						<input type="text" name="addres[0][value]" value="" placeholder="">
+						<label>Комментарий</label>
+						<input type="text" name="addres[0][note]" value="" placeholder="">
+					</div>
+					<?php endif;?>
+				</div>
+				<button class="save" name="workshop_id" value="<?php print $value->workshop_id; ?>" type="submit">Сохранить</button>
+			</form>
+		</div>
+	<?php endforeach;?>
+</div>
 <?php if($mess = SystemMessage::get($main)) print $mess; ?>
 <?php if($rules) : ?>
 <?php var_dump($ws); ?>
