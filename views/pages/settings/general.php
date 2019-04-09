b2eaf5bbe1df1cf58436da712c7bd1b029b94a77
salitygen <salitygen@yandex.ru>
2019-04-09 11:26:08 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -86,6 +86,6 @@ $ws = Service::get($main->session->group_service_id);
 			</div>
 			<?php endif;?>
 		</div>
-		<button class="save" name="companyId" value="<?php print $main->session->user_workshops_id; ?>" type="submit">Сохранить</button>
+		<button class="save" name="companyId" value="<?php print $main->session->group_service_id; ?>" type="submit">Сохранить</button>
 	</form>
 </div>
