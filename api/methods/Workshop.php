d524784a53ee8050842ccc2e9462ef1c0dc496e5
salitygen <salitygen@yandex.ru>
2019-04-09 10:58:15 +0300

    Update Workshop.php

--- api/methods/Workshop.php
+++ api/methods/Workshop.php
@@ -50,8 +50,7 @@ class Workshop {
 			workshop_addres_json='{$data->addres}',
 			workshop_phone_json='{$data->phones}',
 			workshop_mail_json='{$data->mails}',
-			workshop_note='{$data->workshop_note}',
-			workshop_service_id={$data->service_id}
+			workshop_note='{$data->workshop_note}'
 			WHERE workshop_id={$data->companyId}
 		");
 
