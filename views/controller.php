ae4372efb2f79853b3df35bec56349f58d3c689a
salitygen <salitygen@yandex.ru>
2019-04-11 11:35:08 +0300

    Update controller.php

--- views/controller.php
+++ views/controller.php
@@ -5,35 +5,27 @@ if($view){
 	$main->view = $view;
 	$main->params = $params;
 	$main->task = $task;
-	$page = Render::view($main);
+	$page = Render::view($main,false,false);
 }else{
 	header("location: /?view=dashboard");
 }
 
 class Render {
 	
-	public function view($main){
-
-		if(!$main->params){
-			$view = $main->root.'/views/pages/'.$main->view.'/default.php';
+	public function view($main,$module,$params){
+		if(!$module){
+			if(!$main->params){
+				$view = $main->root.'/views/pages/'.$main->view.'/default.php';
+			}else{
+				$view = $main->root.'/views/pages/'.$main->view.'/'.$main->params.'.php';
+				$main->params = false;
+			}
 		}else{
-			$view = $main->root.'/views/pages/'.$main->view.'/'.$main->params.'.php';
-			$main->params = false;
+			$view = $main->root.'/views/modules/'.$module.'/default.php';
 		}
 		
 		if(!is_file($view)){
 			return 'view page not exists';
-		}else{
-			ob_start();
-			include($view);
-			return ob_get_clean();
-		}
-	}
-	
-	public function module($main,$module,$params){
-		$view = $main->root.'/views/modules/'.$module.'/default.php';
-		if(!is_file($view)){
-			return 'view module not exists';
 		}else{
 			ob_start();
 			include($view);
