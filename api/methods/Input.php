e8f101db1b409b98aa6201ac9865d8dd994e4351
salitygen <salitygen@yandex.ru>
2019-04-16 14:41:32 +0300

    Update Input.php

--- api/methods/Input.php
+++ api/methods/Input.php
@@ -284,30 +284,36 @@ class Input {
 		
 		$nk = 0;
 		$newData = array();
-		$data = array_values($data);
-		for($k=0;$k<count($data);$k++){
-			$n = 0;
-			foreach($data[$k] as $i => $value){
-				if($i === 'value'){
-					$newData[$k]['value'] = Input::getSanitise($value);
-					$n++;
-				}elseif($i === 'note'){
-					$newData[$k]['note'] = Input::getSanitise($value);
-					$n++;
+		
+		if(is_array($data)){
+			$data = array_values($data);
+			for($k=0;$k<count($data);$k++){
+				$n = 0;
+				foreach($data[$k] as $i => $value){
+					if($i === 'value'){
+						$newData[$k]['value'] = Input::getSanitise($value);
+						$n++;
+					}elseif($i === 'note'){
+						$newData[$k]['note'] = Input::getSanitise($value);
+						$n++;
+					}
+				}
+				if($n != 2){
+					$newData[$k] = array('value'=>'','note'=>'');
+					if($nk > 0){
+						unset($newData[$k]);
+					}
+					$nk++;
 				}
 			}
-			if($n != 2){
-				$newData[$k] = array('value'=>'','note'=>'');
-				if($nk > 0){
-					unset($newData[$k]);
-				}
-				$nk++;
-			}
+			
+			$newData = array_values($newData);
+			return json_encode($newData,JSON_UNESCAPED_UNICODE);
+			
+		}else{
+			return json_encode(array(array('value'=>'','note'=>'')));
 		}
-		
-		$newData = array_values($newData);
-		
-		return json_encode($newData,JSON_UNESCAPED_UNICODE);
+
 		
 	}
 	
