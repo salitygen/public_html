0b3c4ea134ae28afe1b4fb67ca6bb14f74f28a65
salitygen <44565194+salitygen@users.noreply.github.com>
2019-04-21 17:38:13 +0300

    Update main.js

--- panel/js/main.js
+++ panel/js/main.js
@@ -1,22 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 	
-	document.querySelector('#workshopInfo').addEventListener('click', function(){
-		addClass(this,'hide');
-	});
+	var item = document.querySelectorAll('#workshopInfo'),index;
+	
+	addClassById(item,'click','hide');
+	
+	//ADD CLASS on CLICK, HOVER.......
+	function addClassById(elem,type,adClass){
 
-	function addClass(e,add){
-
-		//ADD CLASS on CLICK
-		if(e.className != ''){
-			var arrClass = e.className.split(" ");
-			arrClass.forEach(function(cls,i){
-				if(add != cls){
-					e.className += " " +add;
-				}
+		for(index = 0; index < item.length; index++){
+			item[index].addEventListener(type, function () {
+				addClass(this,adClass);
 			});
-		}else{
-			e.className += add;
 		}
+
+		function addClass(e,add){
+			//ADD CLASS
+			if(e.className != ''){
+				var arrClass = e.className.split(" ");
+				arrClass.forEach(function(cls,i){
+					if(add != cls){
+						e.className += " " +add;
+					}
+				});
+			}else{
+				e.className += add;
+			}
+		}
+		
 	}
 	
 });
