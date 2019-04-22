6211e9dadd619b51d43a736eb6fafd1199549476
salitygen <salitygen@yandex.ru>
2019-04-22 15:25:15 +0300

    refactory

--- panel/index.php
+++ panel/index.php
@@ -7,6 +7,8 @@ include $main->root.'/views/controller.php';
 	<meta charset="utf-8">
 	<link href="/panel/css/style.css" rel="stylesheet">
 	<link href="/panel/css/fontello.css" rel="stylesheet">
+	<script type="text/javascript" src="/panel/js/jquery-3.4.0.min.js"></script>
+	<script type="text/javascript" src="/panel/js/main.js"></script>
 	</head>
 	<body <?php if($main->view == 'settings') print 'class="level2"'; ?>>
 		<div id="leftPanel">
