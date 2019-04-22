3ac4a57128cacab54051d34bf266d3d6b286d324
salitygen <44565194+salitygen@users.noreply.github.com>
2019-04-23 00:42:51 +0300

    Update main.js

--- panel/js/main.js
+++ panel/js/main.js
@@ -3,37 +3,3 @@ $(function(){
 		$(this).addClass('hide');
 	});
 });
-/* document.addEventListener('DOMContentLoaded', function() {
-	
-	addClassById('workshopInfo','click','hide');
-	
-	//================================
-	//================================
-	//ADD CLASS on CLICK, HOVER.......
-	function addClassById(elem,type,adClass){
-
-		var item = document.querySelectorAll("#"+elem),index;
-	
-		for(index = 0; index < item.length; index++){
-			item[index].addEventListener(type, function () {
-				addClass(this,adClass);
-			});
-		}
-
-		function addClass(e,add){
-			//ADD CLASS
-			if(e.className != ''){
-				var arrClass = e.className.split(" ");
-				arrClass.forEach(function(cls,i){
-					if(add != cls){
-						e.className += " " +add;
-					}
-				});
-			}else{
-				e.className += add;
-			}
-		}
-		
-	}
-	
-}); */
