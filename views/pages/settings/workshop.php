d53a0f463a624d0f3e9a1cee5386576d39bcafec
salitygen <salitygen@yandex.ru>
2019-04-23 16:18:55 +0300

    Update workshop.php

--- views/pages/settings/workshop.php
+++ views/pages/settings/workshop.php
@@ -62,7 +62,9 @@ $rules = 1; // прописано жестко
 					</div>
 					<div class="controls">
 						<button class="icon-plus"></button>
+						<?if($k !== 0):?>
 						<button class="icon-cancel"></button>
+						<?php endif;?>
 					</div>
 					<?php endforeach;?>
 					<?php else:?>
