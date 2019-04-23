c1a97a20e5beac138c7a809f46133098ff880631
salitygen <salitygen@yandex.ru>
2019-04-23 17:48:30 +0300

    Update main.js

--- panel/js/main.js
+++ panel/js/main.js
@@ -20,8 +20,7 @@ $(function(){
 	$('.workshopPhones .controls button.icon-plus').click(function(){
 		var workPhones = $(this).parent().parent().parent();
 		var index = parseInt(workPhones.find('.phoneDiv input').last().attr('name').split('[')[1].split(']')[0]) + 1;
-		var inputs = '<hr>';
-			inputs +='<div class="phoneDiv">';
+		var inputs +='<div class="phoneDiv">';
 			inputs +='<label>Телефон</label>';
 			inputs +='<input type="text" name="phones['+index+'][value]" value="">';
 			inputs +='<label>Комментарий</label>';
