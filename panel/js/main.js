19961a67f4446c5e0ca1934d625211ab0b6b88b0
salitygen <salitygen@yandex.ru>
2019-04-23 17:13:30 +0300

    Update main.js

--- panel/js/main.js
+++ panel/js/main.js
@@ -1,5 +1,7 @@
 $(function(){
 	
+	// WORKSHOPS SETTINGS PAGE START
+	
 	$('button.openClose').click(function(){
 		$(this).parent().parent().parent().toggleClass('hide');
 		var input = $(this).parent().parent().parent().find('input[name="workshop_name"]');
@@ -14,5 +16,20 @@ $(function(){
 	$('p.succes').delay(5000).fadeOut('slow', function(){
 		$(this).remove();
 	});
+	
+	$('.workshopPhones .controls button.icon-plus').click(function(){
+		var workPhones = $(this).parent().parent();
+		var index = workPhones.find('.phoneDiv').attr('name').split('[')[1].split(']')[0] + 1;
+		var inputs = '<div class="phoneDiv">';
+			inputs +='<label>Телефон</label>';
+			inputs +='<input type="text" name="phones['+index+'][value]" value="">';
+			inputs +='<label>Комментарий</label>';
+			inputs +='<input type="text" name="phones['+index+'][note]" value="">';
+			inputs +='</div>';
+		workPhones.append(inputs);
+		return false;
+	});
+
+	// WORKSHOPS SETTINGS PAGE END
 	
 });
