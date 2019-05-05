be4f8eafd74f4640b3cfe68ea51f78b793730e5f
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-06 00:41:15 +0300

    Update worker.php

--- views/pages/settings/worker.php
+++ views/pages/settings/worker.php
@@ -7,8 +7,59 @@ $workerUsers = Workers::getUsers();
 <p>Группы</p>
 <?php
 foreach($workerGroups as $key => $value){
+	
 	$group = explode('_',$key)[1];
-	print HTML::Name($main,'groups',$key) .' - '. $key . ' - ' . $value . '<br>';
+	
+	if($group == 'general'){
+		print '<h3>Основные</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'tasks'){
+		print '<h3>Задачи</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'orders'){
+		print '<h3>Заказы</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'shop'){
+		print '<h3>Магазин</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'tills'){
+		print '<h3>Кассы</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'account'){
+		print '<h3>Счета</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'returns'){
+		print '<h3>Возвраты</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'storage'){
+		print '<h3>Склад</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'contractor'){
+		print '<h3>Контрагенты</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'reports'){
+		print '<h3>Отчеты</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}elseif($group == 'settings'){
+		print '<h3>Настройки</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}else{
+		print '<h3>Прочее</h3>';
+		print HTML::Name($main,'groups',$key) .' - ' . $value . '<br>';
+		
+	}
+
 }
 
 //group_id
