44fff588d8d47733145c55a792fb5b26dc7e35d1
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-05 23:44:48 +0300

    Update worker.php

--- views/pages/settings/worker.php
+++ views/pages/settings/worker.php
@@ -3,14 +3,19 @@ defined('EXEC') or die;
 Rules::settingsWorkers($main) or die('Access Denied');
 $workerGroups = Workers::getGroups();
 $workerUsers = Workers::getUsers();
-
-
+?> 
+<p>Группы</p>
+<?php
 foreach($workerGroups as $key => $value){
 	print HTML::Name($main,'groups',$key) . ' - ' . $value . '<br>';
 }
 
+//group_id
+//group_service_id
+//group_name
+//group_desc
+
 //var_dump($workerGroups);
 //var_dump($workerUsers);
-?> 
-
+?>
 <p>Сотрудники</p>
