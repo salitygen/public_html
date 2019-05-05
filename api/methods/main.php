6b312b6696124ad8ca2e8e1fc7b322264f3cda0c
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-05 18:28:01 +0300

    Update main.php

--- api/methods/main.php
+++ api/methods/main.php
@@ -1,5 +1,6 @@
 <?php
 defined('EXEC') or die;
+include $main->root.'/api/methods/Language.php';
 include $main->root.'/api/methods/Input.php';
 include $main->root.'/api/methods/User.php';
 include $main->root.'/api/methods/Session.php';
