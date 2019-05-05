2014a52349fa37bff78f4a5737891022a5046d5f
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-05 18:57:35 +0300

    Update worker.php

--- views/pages/settings/worker.php
+++ views/pages/settings/worker.php
@@ -4,7 +4,7 @@ Rules::settingsWorkers($main) or die('Access Denied');
 $workerGroups = Workers::getGroups();
 $workerUsers = Workers::getUsers();
 
-print HTML::Name($main,'groups1','GROUP_ID');
+print HTML::Name($main,'groups','GROUP_ID0');
 //var_dump($main);
 var_dump($workerGroups);
 var_dump($workerUsers);
