9e62b8f21fd859f026c19e7029ebf411268e88ee
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-07 00:05:36 +0300

    Update worker.php

--- views/pages/settings/worker.php
+++ views/pages/settings/worker.php
@@ -41,12 +41,14 @@ foreach($workerGroups as $key => $value){
 	<div class="slideBlock hide">
 		<form>
 		<?php //print  $data->id; ?>
-		<?php $n = 0;
-			foreach($data->checkBoxses as $key => $checkbox){
-				print '<div id="bid'.$n.'" class="chkblock"><h3>'. HTML::Name($main,'groups',$key) .'</h3>'.$checkbox.'</div>';
-				$n++;
-			}
-		?>
+			<div class="listChkBlocks">
+			<?php $n = 0;
+				foreach($data->checkBoxses as $key => $checkbox){
+					print '<div id="bid'.$n.'" class="chkblock"><h3>'. HTML::Name($main,'groups',$key) .'</h3>'.$checkbox.'</div>';
+					$n++;
+				}
+			?>
+			</div>
 		</form>
 	</div> 
 <?php } ?>
