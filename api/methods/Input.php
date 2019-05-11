a54c8506f81ec49ac1da5f4432b867d9398a7f6c
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-11 18:55:59 +0300

    Update Input.php

--- api/methods/Input.php
+++ api/methods/Input.php
@@ -335,6 +335,168 @@ class Input {
 		
 	}
 	
+	public function group($data){
+		
+		if(isset($data->group_id)){
+			$data->group_id = (int)Input::postSanitise($data->group_id);
+			if($data->group_id == 0){
+				die('Error in Data!');
+			}
+		}else{
+			die('Error in Data!');
+		}
+		
+		if(isset($data->group_service_id)){
+			$data->group_service_id = (int)Input::postSanitise($data->group_service_id);
+		}else{
+			$data->group_service_id = 0;
+		}
+		
+		if(isset($data->group_name)){
+			$data->group_name = Input::postSanitise($data->group_name);
+		}else{
+			$data->group_name = '';
+		}
+		
+		if(isset($data->group_desc)){
+			$data->group_desc = Input::postSanitise($data->group_desc);
+		}else{
+			$data->group_desc = '';
+		}
+		
+		if(isset($data->ch)){
+			if(!is_array($data->ch)){
+				unset($data->ch);
+				$data->ch = array();
+			}
+		}else{
+			$data->ch = array();
+		}
+		
+		$values = array(
+			'group_super_users',
+			'group_general_see_purchase_price',
+			'group_general_set_discounts_in_doc',
+			'group_general_specify_the_discount_type',
+			'group_general_ask_cost_in_documents',
+			'group_general_list_of_managers',
+			'group_general_list_of_workers',
+			'group_general_company_performance',
+			'group_tasks_create',
+			'group_tasks_of_workers',
+			'group_tasks_see_all',
+			'group_tasks_edit_all',
+			'group_orders_create',
+			'group_orders_moving',
+			'group_orders_delete',
+			'group_orders_app_manager_worker',
+			'group_orders_see_info_customer',
+			'group_orders_edit_field_info',
+			'group_orders_edit_field_works_materials',
+			'group_orders_add_services_price',
+			'group_orders_add_new_services_price',
+			'group_orders_add_materials_stock',
+			'group_orders_add_new_materials_stock',
+			'group_orders_edit_price_service',
+			'group_orders_edit_price_materials',
+			'group_orders_edit_close_order',
+			'group_orders_see_all',
+			'group_shop_create_sales',
+			'group_shop_delete_sales',
+			'group_shop_edit_price',
+			'group_shop_export_sales',
+			'group_tills_make_money',
+			'group_tills_move_money',
+			'group_tills_spend_money',
+			'group_tills_make_move_spend_money_hindsight',
+			'group_tills_see_movement_money',
+			'group_tills_see_movement_money_all_date',
+			'group_tills_delete_transaction',
+			'group_tills_print_movement_money',
+			'group_tills_export_movement_money',
+			'group_tills_see_rest_money',
+			'group_tills_add_edit_remove_cash',
+			'group_tills_select_workers_list',
+			'group_account',
+			'group_returns_create',
+			'group_returns_delete',
+			'group_returns_edit_price',
+			'group_storage_tailings',
+			'group_storage_posting',
+			'group_storage_offs',
+			'group_storage_move',
+			'group_storage_category',
+			'group_storage_add_create_delete',
+			'group_storage_delete_operations',
+			'group_storage_edit_article_card',
+			'group_storage_on_serial_accounting_article',
+			'group_storage_export_article',
+			'group_storage_import_article',
+			'group_contractor_see_customer',
+			'group_contractor_see_supplier',
+			'group_contractor_edit_card',
+			'group_contractor_edit_discount_customer',
+			'group_contractor_export_data',
+			'group_contractor_import_data',
+			'group_reports_data_print',
+			'group_reports_data_export',
+			'group_reports_data_create_all_date',
+			'group_reports_salary',
+			'group_reports_money',
+			'group_reports_cash_flow',
+			'group_reports_profit_orders',
+			'group_reports_profit_sales',
+			'group_reports_create_orders',
+			'group_reports_close_orders',
+			'group_reports_work_orders',
+			'group_reports_source_orders',
+			'group_reports_performers',
+			'group_reports_work',
+			'group_reports_trade_turnover',
+			'group_reports_remains_warehouse',
+			'group_reports_offs_warehouse',
+			'group_reports_requiring_purchase',
+			'group_reports_calls',
+			'group_reports_send_sms',
+			'group_reports_delayed_sms',
+			'group_reports_client_feedback',
+			'group_reports_history_login',
+			'group_reports_reports_salary_only',
+			'group_reports_reports_salary_all',
+			'group_settings_general',
+			'group_settings_integration',
+			'group_settings_workshop',
+			'group_settings_service',
+			'group_settings_users',
+			'group_settings_statuses',
+			'group_settings_groups',
+			'group_settings_rules',
+			'group_settings_sale_prices',
+			'group_settings_form_designer',
+			'group_settings_list_services',
+			'group_settings_handbook',
+			'group_settings_document_template',
+			'group_settings_notification',
+			'group_settings_api',
+			'group_settings_subscription',
+			'group_settings_partner_program'
+		);
+		
+		$data->checkbox = array();
+		
+		foreach($values as $value){
+			if(isset($data->ch[$value])){
+				$data->checkbox[$value] = (int)Input::postSanitise($data->ch[$value]);
+			}else{
+				$data->checkbox[$value] = 0;
+			}
+		}
+		
+		unset($data->ch);
+		return $data;
+		
+	}
+	
 	
 }
 
