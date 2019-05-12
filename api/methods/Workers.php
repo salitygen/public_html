9ab274c2a33c67c499bc3c3ef8e8384de8eb987e
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-12 13:53:24 +0300

    Update Workers.php

--- api/methods/Workers.php
+++ api/methods/Workers.php
@@ -7,18 +7,17 @@ class Workers {
 		
 		if(isset($data['login']) && isset($data['password'])){
 			
-			if(!is_array($data['login']) || !is_array($data['password'])){
-				if(mb_strlen((string)$data['login']) > 50 || mb_strlen((string)$data['password']) > 50){
-					return false;
-				}
+			$db = dataBase::pdo();
+			
+			$login = Input::postSanitise($data['login']);
+			$pass = Input::postSanitise($data['password']);
+			
+			if(mb_strlen($pass) <= 50){
+				$pass = md5($pass);
 			}else{
 				return false;
 			}
 			
-			$db = dataBase::pdo();
-			
-			$login = Input::postSanitise($data['login']);
-			$pass = md5(Input::postSanitise($data['password']));
 			$searchUser = $db->query("SELECT * FROM crm_users WHERE user_login='{$login}' AND user_pass='{$pass}'");
 			$user = $searchUser->fetch();
 
