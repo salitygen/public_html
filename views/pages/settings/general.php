373559f99a5749b91ec1036393fb4001234e4006
salitygen <44565194+salitygen@users.noreply.github.com>
2019-05-12 15:43:16 +0300

    Update general.php

--- views/pages/settings/general.php
+++ views/pages/settings/general.php
@@ -1,11 +1,11 @@
 <?php
 defined('EXEC') or die;
 Rules::settingsGeneral($main) or die('Access Denied');
-$ws = General::get();
+$company = General::get();
 ?>
 <p>Информация о компании</p>
 <div class="companyStatus">
-<?php if($ws->general_status):?>
+<?php if($company->general_status):?>
 	<p>Работает</p>
 <?php else:?>
 	<p class="off">Не работает</p>
@@ -15,20 +15,20 @@ $ws = General::get();
 <div class="slideBlock">
 	<form action="/?view=settings&params=general&task=update" method="POST">
 		<div class="center">
-		  <input type="checkbox" name="general_status" id="cbx" style="display:none" <?php if($ws->general_status) print 'checked="true"'; ?> >
+		  <input type="checkbox" name="general_status" id="cbx" style="display:none" <?php if($company->general_status) print 'checked="true"'; ?> >
 		  <label for="cbx" class="toggle"><span></span></label>    
 		</div>
 		<div class="companyName">
-			<input type="text" required="required" name="general_name" value="<?php if($ws->general_name != '') print $ws->general_name; ?>">
+			<input type="text" required="required" name="general_name" value="<?php if($company->general_name != '') print $company->general_name; ?>">
 		</div>
 		<div class="companyNote">
 			<label>Дополнительная информация</label>
-			<textarea name="general_note" ><?php if($ws->general_note != '') print $ws->general_note; ?></textarea>
+			<textarea name="general_note" ><?php if($company->general_note != '') print $company->general_note; ?></textarea>
 		</div>
 		<div class="companyPhones">
 			<label>Номера телефонов</label>
-			<?php if($ws->phones) : ?>
-			<?php foreach($ws->phones as $k => $phone):?>
+			<?php if($company->phones) : ?>
+			<?php foreach($company->phones as $k => $phone):?>
 			<div class="phoneDiv">
 				<label>Телефон</label>
 				<input type="text" name="phones[<?php print $k; ?>][value]" value="<?php print $phone->value; ?>">
@@ -47,8 +47,8 @@ $ws = General::get();
 		</div>
 		<div class="companyMails">
 			<label>Электронная почта</label>
-			<?php if($ws->mails) : ?>
-			<?php foreach($ws->mails as $k => $mail):?>
+			<?php if($company->mails) : ?>
+			<?php foreach($company->mails as $k => $mail):?>
 			<div class="mailDiv">
 				<label>Почтовый ящик</label>
 				<input type="text" name="mails[<?php print $k; ?>][value]" value="<?php print $mail->value; ?>">
@@ -67,8 +67,8 @@ $ws = General::get();
 		</div>
 		<div class="companyAddres">
 			<label>Адреса компании</label>
-			<?php if($ws->addres) : ?>
-			<?php foreach($ws->addres as $k => $adres):?>
+			<?php if($company->addres) : ?>
+			<?php foreach($company->addres as $k => $adres):?>
 			<div class="addresDiv">
 				<label>Адрес</label>
 				<input type="text" name="addres[<?php print $k; ?>][value]" value="<?php print $adres->value; ?>">
