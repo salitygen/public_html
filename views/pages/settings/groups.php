e1e4bb02d6f7ff125ec557ee4e11b45032d526ef
salitygen <salitygen@yandex.ru>
2019-05-13 16:54:50 +0300

    Update groups.php

--- views/pages/settings/groups.php
+++ views/pages/settings/groups.php
@@ -36,7 +36,7 @@ if(isset($_GET['id'])){
 						
 						$name = explode('_',$key)[1];
 						print $key.'<br>';
-						print next($group);
+						print key(next($group)).'</br>';
 						//print key(next($group)).'<br>';
 						
 /* 						if($name != 'id'
