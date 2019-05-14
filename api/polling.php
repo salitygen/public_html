e5dd601d0ea3a93b79024edb141142294b1276e6
salitygen <salitygen@yandex.ru>
2019-05-14 17:19:23 +0300

    Update polling.php

--- api/polling.php
+++ api/polling.php
@@ -6,29 +6,33 @@ if(isset($_GET['poll'])){
 	include $_SERVER['DOCUMENT_ROOT'].'/config/config.php';
 	include $main->root.'/config/pdo.php';
 	include $main->root.'/api/controller.php';
+	
+	if(defined('ISLOGIN')){
 
-	$count = (int)Input::getSanitise($_GET['poll']);
-	$timeout = 25;
-	$now = time();
-	$db = dataBase::pdo();
+		$count = (int)Input::getSanitise($_GET['poll']);
+		$timeout = 25;
+		$now = time();
+		$db = dataBase::pdo();
 
-	while((time() - $now) < $timeout){
-		
-		$row = $db->query("SELECT * FROM crm_users");
-		$data = $row->rowCount();
-		
-		if($data != $count){
+		while((time() - $now) < $timeout){
 			
-			print $data;
-			exit;
+			$row = $db->query("SELECT * FROM crm_users");
+			$data = $row->rowCount();
+			
+			if($data != $count){
+				
+				print $data;
+				exit;
+				
+			}
+			
+			sleep(1);
 			
 		}
-		
-		sleep(1);
+
+		print 'next';
 		
 	}
-
-	print 'next';
 	
 }
 
