53bdbf90a290cc0c0f1357ff8c47b6dfb9d794fb
salitygen <salitygen@yandex.ru>
2019-05-14 18:16:10 +0300

    Update polling.js

--- panel/js/polling.js
+++ panel/js/polling.js
@@ -3,31 +3,32 @@ $(function(){
 	function pool(){
 
 		var count = $.cookie('count').trim();
-			var xhr = $.ajax({
-				async:true,
-				cache: false,
-				url: '/api/polling.php?poll='+count,
-				success: function(data){
-					if(data.trim() != 'next'){
-						if(count != data.trim()){
-							alert(data.trim());
-							$.cookie('count',data.trim(),{ path: '/' });
-							setTimeout(pool, 1000);
-						}
-					}else{
+		var xhr = $.ajax({
+			async:true,
+			cache: false,
+			url: '/api/polling.php?poll='+count,
+			success: function(data){
+				if(data.trim() != 'next'){
+					if(count != data.trim()){
+						alert(data.trim());
+						$.cookie('count',data.trim(),{ path: '/' });
 						setTimeout(pool, 1000);
-					}	
-				},
-				error: function(){
-					setTimeout(pool, 10000);
-				}
-			});
-		}
+					}
+				}else{
+					setTimeout(pool, 1000);
+				}	
+			},
+			error: function(){
+				setTimeout(pool, 10000);
+			}
+		});
+		
+		$('a').click(function(){
+			xhr.abort();
+		});
+		
+	}
 	
 	setTimeout(pool, 1000);
-	
-	$('a').click(function(){
-		xhr.abort();
-	})
 	
 });
