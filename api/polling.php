f870195c624ad99beebdca2ae87e2144f82d6b74
salitygen <salitygen@yandex.ru>
2019-05-14 18:26:06 +0300

    Update polling.php

--- api/polling.php
+++ api/polling.php
@@ -19,6 +19,13 @@ if(isset($_GET['poll'])){
 
 		while((time() - $now) < $timeout){
 			
+			if(isset($_COOKIE['abort'])){
+				$abort = (int)$_COOKIE['abort'];
+				if($abort == 1){
+					exit;
+				}
+			}
+			
 			$row = $db->query("SELECT * FROM crm_users");
 			$data = $row->rowCount();
 			
