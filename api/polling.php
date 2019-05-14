b65329b4c5e14d3bffd4ba94a81da5b435fbe760
salitygen <salitygen@yandex.ru>
2019-05-14 16:39:22 +0300

    Update polling.php

--- api/polling.php
+++ api/polling.php
@@ -1,26 +1,26 @@
 <?php
 defined('EXEC') or die;
 
-	//$count = (int)Input::getSanitise($_GET['poll']);
-	//$timeout = 25;
-	//$now = time();
-	//$db = dataBase::pdo();
+	$count = (int)Input::getSanitise($_GET['poll']);
+	$timeout = 25;
+	$now = time();
+	$db = dataBase::pdo();
 
-	//while((time() - $now) < $timeout){
+	while((time() - $now) < $timeout){
 		
-		//$row = $db->query("SELECT * FROM db_updating_table");
-		//$data = $row->rowCount();
+		$row = $db->query("SELECT * FROM db_updating_table");
+		$data = $row->rowCount();
 		
-		//if($data != $count){
+		if($data != $count){
 			
-			//print $data;
-			//exit;
+			print $data;
+			exit;
 			
-		//}
+		}
 		
-		//sleep(1);
+		sleep(1);
 		
-	//}
+	}
 
 	print 'next';
 	
