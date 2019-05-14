7b2098f181901289a8c44b1320f5ed730c78918e
salitygen <salitygen@yandex.ru>
2019-05-14 16:05:22 +0300

    Update polling.js

---
+++ panel/js/polling.js
@@ -0,0 +1,29 @@
+$(function(){
+	
+	function pool(){
+		
+		var count = $.cookie('count');
+			$.ajax({
+				async:true,
+				cache: false,
+				url: '/api/polling.php?count='+count,
+				success: function(data){
+					if(data != 'next'){
+						if(count != data){
+							alert(data);
+							$.cookie('count',data);
+							pool();
+						}
+					}else{
+						setTimeout(pool, 1000);
+					}	
+				},
+				error: function(){
+					setTimeout(pool, 10000);
+				}
+			});
+		}
+	
+	pool();
+	
+});
