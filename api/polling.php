d6d03887aa7219cd58c398493e496098fb19a7cf
salitygen <salitygen@yandex.ru>
2019-05-14 17:54:24 +0300

    Update polling.php

--- api/polling.php
+++ api/polling.php
@@ -4,20 +4,11 @@ session_start();
 
 if(isset($_GET['poll'])){
 	
+	define('POLLING',1);
+	
 	include $_SERVER['DOCUMENT_ROOT'].'/config/config.php';
 	include $main->root.'/config/pdo.php';
 	include $main->root.'/api/controller.php';
-	
-/* 	$sessionHash = Session::getHash();
-	if($sessionHash){
-		$session = Session::check($sessionHash);
-		if($session){
-			if($session->session_stat && $session->user_status){
-				$main->session = $session;
-				define('ISLOGIN',1);
-			}
-		}
-	} */
 	
 	if(defined('ISLOGIN')){
 
